fbc73abfdd0e7d1a084709580388fd32edd65a15
whoisclebs <43012757+whoisclebs@users.noreply.github.com>
2025-02-24 00:03:16 -0300

    chore: initialize Docusaurus project with basic configuration and deploy setup

---
+++ docs/intro.md
@@ -0,0 +1,19 @@
+---
+sidebar_position: 1
+---
+
+# About these notes
+
+This repository is a reflection of my commitment to continuous learning and personal growth. It serves as a structured way to document my journey, allowing me to revisit key milestones, track my progress, and consolidate my knowledge over time.
+
+All the notes and guides are carefully organized to facilitate understanding and provide a step-by-step approach to various topics. 
+
+## How It Works
+
+The documentation is built using **Docusaurus**, a  React-based static site generator optimized for technical content and documentation, and is deployed on **GitHub Pages**. 
+
+You can explore the repository here: [technotes repository](https://github.com/whoisclebs/technotes).
+
+## License Summary
+
+This project is open-source and distributed under the **MIT License**, allowing for free use, modification, and distribution with minimal restrictions. For more details, refer to the full license text included in the repository.
---
+++ docusaurus.config.ts
@@ -0,0 +1,77 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'WhoIsClebs - TechNotes',
+  tagline: 'Dinosaurs are cool',
+  favicon: 'img/icon.png',
+  url: 'https://technotes.whoisclebs.com',
+  baseUrl: '/',
+  organizationName: 'whoisclebs',
+  projectName: 'technotes',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'pt-BR'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    navbar: {
+      title: 'TechNotes',
+      logo: {
+        alt: 'TechNotes Logo',
+        src: 'img/icon.png',
+        srcDark: 'img/icon.png',
+      },
+      items: [
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'GitHub',
+              href: 'https://github.com/whoisclebs/technotes',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} TechNotes. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
